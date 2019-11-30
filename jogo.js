ec0b81605fd6a81ac2e00ee9ef75e332ad7687cd
JulianaMac <juliana.s.machado@hotmail.com>
2019-11-30 16:09:16 -0300

    add testes unitarios

--- jogo.js
+++ jogo.js
@@ -1,4 +1,8 @@
 function jogo(numeroGerado, resposta) {
+    const numeroFormatado = parseInt(resposta)
+    if (Number.isNaN(numeroFormatado)) {
+      return { mensagemErro: 'Não é um número válido' }
+    }
     if (numeroGerado == resposta){
 
         return{
